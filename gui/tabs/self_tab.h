5d9d8ebb1918ddaaf4f8e16204691aa8e07d8488
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2023-12-10 21:55:19 +1000

    fake roles part 2

--- gui/tabs/self_tab.h
+++ gui/tabs/self_tab.h
@@ -1,5 +1,6 @@
 #pragma once
 
 namespace SelfTab {
+    const std::vector<const char*> FAKEROLES = { "Crewmate", "Scientist", "Engineer", "Impostor", "Shapeshifter", "Crew Ghost", "Guardian Angel", "Imp Ghost" };
 	void Render();
 }
