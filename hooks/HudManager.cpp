3d2b5e4f799719d4f5ccc10b47f0cc1b9db7096e
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-01-07 14:35:12 +0530

    help wanted

--- hooks/HudManager.cpp
+++ hooks/HudManager.cpp
@@ -68,15 +68,3 @@ void dVersionShower_Start(VersionShower* __this, MethodInfo* method) {
 	app::TMP_Text_set_alignment((app::TMP_Text*)__this->fields.text, app::TextAlignmentOptions__Enum::TopLeft, nullptr);
 	app::TMP_Text_set_text((app::TMP_Text*)__this->fields.text, convert_to_string(versionText), nullptr);
 }
-
-void dPingTracker_Update(PingTracker* __this, MethodInfo* method) {
-    app::PingTracker_Update(__this, method);
-    try {
-        std::string pingText = std::format("{}\nTest", ping);
-        app::TMP_Text_set_alignment((app::TMP_Text*)__this->fields.text, app:TextAlignmentOptions__Enum::TopRight, nullptr);
-        app::TMP_Text_set_text((app::TMP_Text*)__this->fields.text, convert_to_string(pingText), nullptr);
-    }
-    catch (...) {
-        LOG_DEBUG("Exception occured in PingTracker_Update (HudManager.cpp)");
-    }
-}
